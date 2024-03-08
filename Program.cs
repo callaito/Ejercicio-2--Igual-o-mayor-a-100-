e518f6f22b7458f76ceac23d81e8c9641cf07eee
callaito <radelinrsm029@gmail.com>
2024-03-07 21:39:29 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ejercicio 2");
+Console.WriteLine();
+
+
+
+int numero, sumatoria =0;
+
+numero = Convert.ToInt32(Console.ReadLine());
+//Inicializar siempre antes del bucle
+
+
+
+
+while (sumatoria <= 100)
+{
+
+   
+    sumatoria += numero;
+
+}
+
+
+//Calcular el sumatoria
+
+if (sumatoria <= 100 )
+{
+    Console.WriteLine("El numero es igual o mayor a 100: " + sumatoria );
+}
+
+else
+{
+    Console.WriteLine("El numero es menor");
+}
